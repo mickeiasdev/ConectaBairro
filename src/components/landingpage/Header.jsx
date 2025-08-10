4cbcf7c5ce1f83e9cd7382ced4466dbd44880a50
Mickeias Dev <mickeiasdev@gmail.com>
2025-08-10 13:18:24 -0300

    feat: enhance Header component with responsive menu and improve Footer padding

--- src/components/landingpage/Header.jsx
+++ src/components/landingpage/Header.jsx
@@ -1,17 +1,48 @@
+import { useState } from "react";
+
+// components
 import { PrimaryButton } from "../../components/Buttons";
 
+// icons
+import { SlMenu } from "react-icons/sl";
+
+// styles
 import styles from "../../styles/landingpage/Header.module.css";
 
 export function Header({ className, home, features, tutorial, feedback }) {
+  const [nav, useNav] = useState();
+  const [content, useContent] = useState();
+
+  const handleMenu = () => {
+    if (nav === `${styles.nav}`) {
+      useNav(`${styles.nav} ${styles.active}`);
+      useContent(`${styles.content} ${styles.active}`);
+    } else {
+      useNav(`${styles.nav}`);
+      useContent(`${styles.content}`);
+    }
+  };
+
   return (
     <div className={`${styles.header} ${className}`}>
-      <div className={styles.nav}>
-        <button onClick={home}>Inicio</button>
-        <button onClick={features}>Serviços</button>
-        <button onClick={tutorial}>Como usar</button>
-        <button onClick={feedback}>Oque dizem</button>
+      <SlMenu className={styles.menu} onClick={handleMenu} />
+      <div className={nav}>
+        <div>
+          <button className={content} onClick={home}>
+            Inicio
+          </button>
+          <button className={content} onClick={features}>
+            Serviços
+          </button>
+          <button className={content} onClick={tutorial}>
+            Como usar
+          </button>
+          <button className={content} onClick={feedback}>
+            Oque dizem
+          </button>
+        </div>
+        <PrimaryButton title="Entrar" style={`${styles.button} ${content}`} />
       </div>
-      <PrimaryButton title="Entrar" style={styles.button}/>
     </div>
   );
 }
